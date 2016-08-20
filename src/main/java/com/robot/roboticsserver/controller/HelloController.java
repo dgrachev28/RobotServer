469a48dcad93c7f08ece4ec61449e64e5f25e23a
Denis Grachev <dgrachev28@gmail.com>
2016-08-20 11:50:01 +0300

    Initial commit

---
+++ src/main/java/com/robot/roboticsserver/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.robot.roboticsserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+}
