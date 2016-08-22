cb63d66a4ba436dbb7f77780eedf173b41503fb4
DIGrachev <ltybC555>
2016-08-22 19:18:28 +0300

    Application and security config

---
+++ src/main/java/com/robot/roboticsserver/configuration/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.robot.roboticsserver.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MessageSecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/robot/roboticsserver/configuration/RootConfig.java
@@ -0,0 +1,9 @@
+package com.robot.roboticsserver.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RootConfig {
+}
